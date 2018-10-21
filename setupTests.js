583da6dcae9d87e11fd0b5ad1c46945100890333
William Corry <william@wrought-webdesign.co.uk>
2018-10-21 18:18:05 +0100

    fix(Tests): Resolved unhandled fetch error when running jest and added types

--- setupTests.js
+++ setupTests.js
@@ -1,4 +1,11 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+const fetchPolifill = require('whatwg-fetch');
+
+global.fetch = fetchPolifill.fetch;
+global.Request = fetchPolifill.Request;
+global.Headers = fetchPolifill.Headers;
+global.Response = fetchPolifill.Respons;
+
 configure({ adapter: new Adapter() });
